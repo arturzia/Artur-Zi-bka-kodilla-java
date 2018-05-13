cab170f1e58c49ebcf9c48fdd59f6bdfe2c8096f
arturzia <arturzia@gmail.com>
2018-05-13 20:44:39 +0200

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
 
         public static void main(String[] args){
-            System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+            // Testing Calculator class
+            Calculator calculator = new Calculator();
+            //first method
+            int addResult = calculator.add(134, 11);
+            if (addResult == 145) {
+                System.out.println("Calculator test OK");
+            } else {
+                System.out.println("Calculator has error!");
+            }
+            //second method
+            int subResult = calculator.subtract(120, 90);
+            if (subResult == 30) {
+                System.out.println("Calculator test OK");
+            } else {
+                System.out.println("Calculator has error!");
+            }
         }
 }
 
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int add(int a, int b){
+
+        return a + b;
+    }
+
+    public int subtract (int a, int b){
+
+        return a - b;
+    }
+
+
+
+}
