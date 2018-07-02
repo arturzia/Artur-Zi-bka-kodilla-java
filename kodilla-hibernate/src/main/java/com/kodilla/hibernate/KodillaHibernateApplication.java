c98bc0e4d94261e47cd87b9236fc3037ee403905
arturzia <arturzia@gmail.com>
2018-07-02 18:00:41 +0200

    Task_13.1

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication (exclude = { DataSourceAutoConfiguration.class })
+public class KodillaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernateApplication.class, args);
+    }
+}
